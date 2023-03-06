eae10db0097db5dfaf22f8ba0709d5540a801c5f
Ebikakpou <ebikakpou@gmail.com>
2023-03-05 18:20:51 -0800

    Django ecommerce

---
+++ ecomprj/core/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+app_name = "core"
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+    path('products/',views.product_list_view, name='product-list'),
+    path('products/<pid>/', views.product_detail_view, name='product-detail'),
+
+    path('category/', views.category_list_view, name='category-list'),
+    path('category/<cid>/', views.category_product_list_view, name='category-product-list'),
+
+    path('vendors/', views.vendor_list_view, name="vendor-list"),
+    path('vendor/<vid>/', views.vendor_detail_view, name="vendor-details"),
+
+    path('products/tag/<slug:tag_slug>/', views.tag_list, name='tags'),
+
+    path('ajax-add-review/<int:pid>/', views.ajax_add_review, name='ajax-add-review'),
+
+    path('search/', views.search_view, name="search"),
+
+    path('filter-products/', views.filter_product, name='filter-product'),
+
+]
---
+++ ecomprj/core/views.py
@@ -0,0 +1,196 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render,redirect, get_object_or_404
+from django.db.models import Count, Avg 
+from taggit.models import Tag 
+from stripe import review 
+from core.models import CartOrderProducts, Product, Category, Vendor, CartOrder, ProductImages, ProductReview, wishlist_model, Address  
+from core.forms import ProductReviewForm
+from django.template.loader import render_to_string
+
+# Create your views here.
+
+
+def index(request):
+    #products = Product.objects.all().order_by("-id")
+    products = Product.objects.filter(product_status = "published", featured=True)
+
+    context = {
+        "products":products
+    }
+    return render(request, 'core/index.html', context)
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status = "published")
+
+    context = {
+        "products":products
+    }
+    return render(request, 'core/product-list.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.all()
+
+
+    context = {
+        "categories":categories
+    }
+
+    return render(request, 'core/category-list.html', context)
+
+
+
+def category_product_list_view(request, cid):
+    category = Category.objects.get(cid=cid)
+    products = Product.objects.filter(product_status="published", category=category)
+
+    context = {
+        "category":category,
+        "products":products
+    }
+
+    return render(request, 'core/category-product-list.html', context)
+
+
+def vendor_list_view(request):
+    vendors = Vendor.objects.all()
+    context = {
+        "vendors":vendors,
+    }
+
+    return render(request, "core/vendor-list.html", context)
+
+
+
+def vendor_detail_view(request, vid):
+    vendor = Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status="published")
+
+    context = {
+        "vendor":vendor,
+        "products":products,
+    }
+
+    return render(request, "core/vendor-detail.html", context)
+
+def product_detail_view(request, pid):
+    product = Product.objects.get(pid=pid)
+    products = Product.objects.filter(category=product.category).exclude(pid=pid)
+   
+
+    #Getting reviews
+
+    reviews = ProductReview.objects.filter(product=product).order_by("-date")
+
+    #Getting average rating
+
+    average_rating = ProductReview.objects.filter(product=product).aggregate(rating=Avg('rating'))
+
+    # Product Review Form
+
+    review_form = ProductReviewForm()
+
+    make_review = True
+
+    if request.user.is_authenticated:
+        user_review_count = ProductReview.objects.filter(user=request.user, product=product).count()
+
+        if user_review_count > 0:
+            make_review = False
+
+    p_image = product.p_images.all()
+
+    context = {
+        "p": product,
+        "make_review": make_review,
+        "review_form": review_form,
+        "p_image":p_image,
+        "average_rating": average_rating,
+        "reviews": reviews,
+        "products":products,
+    }
+
+    return render(request, "core/product-detail.html", context)
+
+
+def tag_list(request, tag_slug="None"):
+    products = Product.objects.filter(product_status="published").order_by("-id")
+
+    tag = None 
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        products = products.filter(tags__in=[tag])
+
+    context = {
+        "products":products,
+        "tag":tag,
+    }
+
+    return render(request, "core/tag.html", context)
+
+
+def ajax_add_review(request, pid):
+    product = Product.objects.get(pk=pid)
+    user = request.user
+
+    review = ProductReview.objects.create(
+        user=user,
+        product=product,
+        review=request.POST['review'],
+        rating =request.POST['rating'],
+    )
+
+    context = {
+        'user': user.username,
+        'review': request.POST['review'],
+        'rating': request.POST['rating'],
+    }
+
+    average_reviews = ProductReview.objects.filter(product=product).aggregate(rating=Avg("rating"))
+
+    return JsonResponse(
+       { 
+        'bool':True,
+        'context': context,
+        'average_reviews': average_reviews,
+        }
+    )
+
+
+def search_view(request):
+    query = request.GET.get("q")
+
+    products = Product.objects.filter(title__icontains=query).order_by("-date")
+
+    context = {
+        "products": products,
+        "query": query,
+    }
+
+    return render(request, "core/search.html", context)
+
+
+def filter_product(request):
+    categories = request.GET.getlist("category[]")
+    vendors = request.GET.getlist("vendor[]")
+
+
+    #min_price = request.GET['min_price']
+   # max_price = request.GET['max_price']
+
+    products = Product.objects.filter(product_status="published").order_by("-id").distinct()
+
+   # products = products.filter(price__gte=min_price)
+    #products = products.filter(price__lte=max_price)
+
+
+    if len(categories) > 0:
+        products = products.filter(category__id__in=categories).distinct() 
+
+    if len(vendors) > 0:
+        products = products.filter(vendor__id__in=vendors).distinct() 
+    
+    
+    data = render_to_string("core/async/product-list.html", {"products": products})
+    return JsonResponse({"data": data})
